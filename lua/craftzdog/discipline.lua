f3c1e7181e2190006aba2e11f7b7434848d90571
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-03 23:26:19 -0400

    refactor(discipline): change hjkl limit keys to 40

--- lua/craftzdog/discipline.lua
+++ lua/craftzdog/discipline.lua
@@ -11,12 +11,12 @@ function M.cowboy()
 			if vim.v.count > 0 then
 				count = 0
 			end
-			if count >= 10 and vim.bo.buftype ~= "nofile" then
-        			ok = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
+			if count >= 40 and vim.bo.buftype ~= "nofile" then
+				ok = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
 					icon = "🤠",
 					id = "cowboy",
 					keep = function()
-						return count >= 10
+						return count >= 40
 					end,
 				})
 				if not ok then
