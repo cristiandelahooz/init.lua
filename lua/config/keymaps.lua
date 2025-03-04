994d66d90ebabf9f9f1942fb71377f88e04969ab
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-04 10:21:09 -0400

    feat(keymaps): keymap to paste without yanking

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -37,6 +37,10 @@ keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 keymap.set("n", "J", "mzJ`z") -- Join lines without move cursor
+
+--paste without yanking
+keymap.set("x", "<leader>p", [["_dP]])
+
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
