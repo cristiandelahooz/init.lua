adad16518c363ba8752b28255468acf3d548ec7e
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-03 23:27:21 -0400

    feat(keymaps): add run function keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,6 +19,9 @@ keymap.set("n", "<Leader>D", '"_D')
 keymap.set("v", "<Leader>d", '"_d')
 keymap.set("v", "<Leader>D", '"_D')
 
+-- exit insert mode
+keymap.set("i", "qj", "<Esc>:w<CR>")
+
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
@@ -74,3 +77,89 @@ end)
 vim.api.nvim_create_user_command("ToggleAutoformat", function()
 	require("craftzdog.lsp").toggleAutoformat()
 end, {})
+
+--[[ 
+    Run the current file based on the filetype
+    Supported filetypes:
+    - javascript
+    - typescript
+    - c
+    - go
+    - zsh
+    - bash
+    - haskell
+    - java
+]]
+function RunCurrentFile()
+	local filetype = vim.bo.filetype
+	local filename = vim.fn.expand("%")
+	local output_file = "output"
+
+	local commands = {
+		javascript = "node " .. filename,
+		typescript = "tsx " .. filename,
+		c = "gcc " .. filename .. " -o " .. output_file .. " && ./" .. output_file .. " && rm " .. output_file,
+		go = "go run " .. filename,
+		zsh = "./" .. filename,
+		bash = "./" .. filename,
+		haskell = "ghc " .. filename .. " -o " .. output_file .. "&& ./" .. output_file .. "&& rm ./*{.o,.hi,output}",
+		java = "gd run",
+	}
+
+	if commands[filetype] then
+		vim.cmd(":term " .. commands[filetype]) -- Run program
+	else
+		print("No execution command set for filetype: " .. filetype)
+	end
+end
+
+vim.api.nvim_set_keymap("n", "<leader>r", ":lua RunCurrentFile()<CR>", opts)
+
+local function set_keymaps_for_ft()
+	local ft = vim.bo.filetype
+
+	if ft == "java" then
+		-- Manejo de excepciones en Java
+		vim.keymap.set(
+			"n",
+			"<leader>ee",
+			"otry {<CR>} catch (Exception e) {<CR>throw new RuntimeException(e);<CR>}<Esc>O",
+			{ buffer = true }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>el",
+			'otry {<CR>} catch (Exception e) {<CR>logger.severe("Error: " + e.getMessage());<CR>}<Esc>O',
+			{ buffer = true }
+		)
+		vim.keymap.set("n", "<leader>ea", 'oassert condition : "Error: " + e.getMessage();<Esc>F";a', { buffer = true })
+		vim.keymap.set(
+			"n",
+			"<leader>ef",
+			'otry {<CR>} catch (Exception e) {<CR>System.err.println("Error: " + e.getMessage());<CR>}<Esc>O',
+			{ buffer = true }
+		)
+	elseif ft == "go" then
+		-- Manejo de errores en Go
+		vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { buffer = true })
+		vim.keymap.set("n", "<leader>ea", 'oassert.NoError(err, "")<Esc>F";a', { buffer = true })
+		vim.keymap.set(
+			"n",
+			"<leader>ef",
+			'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj',
+			{ buffer = true }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>el",
+			'oif err != nil {<CR>}<Esc>O.logger.Error("error", "error", err)<Esc>F.;i',
+			{ buffer = true }
+		)
+	end
+end
+
+-- Execute when open a buffer
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	callback = set_keymaps_for_ft,
+})
