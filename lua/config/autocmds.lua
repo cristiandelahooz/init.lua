fd76196d48a1fef09509018d031ea1bb4b9830f3
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-04 19:27:00 -0400

    feat(autocommands): add lsp method in lspattach

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,14 +1,39 @@
+local augroup = vim.api.nvim_create_augroup
+local delahozGroup = augroup("delahoz", {})
+local autocmd = vim.api.nvim_create_autocmd
+
 -- Turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd("InsertLeave", {
+autocmd("InsertLeave", {
+	group = delahozGroup,
 	pattern = "*",
 	command = "set nopaste",
 })
 
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
-vim.api.nvim_create_autocmd("FileType", {
+autocmd("FileType", {
+	group = delahozGroup,
 	pattern = { "json", "jsonc", "markdown" },
 	callback = function()
 		vim.opt.conceallevel = 0
+	end,
+})
+
+autocmd("LspAttach", {
+	group = delahozGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "<leader>f", function()
+			vim.lsp.buf.format()
+		end, opts)
+		vim.keymap.set("n", "<leader>rn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
 	end,
 })
