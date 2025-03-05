cda0934332639b4891616a3e834ef3a898e2376f
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-03-05 16:42:39 -0400

    chore(options): use scpace instead of tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,6 +20,7 @@ vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 vim.opt.inccommand = "split"
 vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
 vim.opt.smarttab = true
+vim.opt.expandtab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
