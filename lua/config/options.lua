9b87a7a5d9790e059778e1a688af486590ac40ef
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-09 14:54:59 -0400

    refactor(options): replace undercurl commands with one command

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -35,9 +35,10 @@ vim.opt.clipboard = ""
 vim.opt.mouse = ""
 
 -- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
-
+vim.cmd([[
+  let &t_Cs = "\e[4:3m"
+  let &t_Ce = "\e[4:0m"
+]])
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
 
