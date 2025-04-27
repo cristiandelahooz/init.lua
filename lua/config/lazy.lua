401df3b5b487a99b7e2b674d1e15d394ca35c2e2
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-26 21:10:51 -0400

    chore: update bootsrap lazy snippet

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,14 +1,17 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local extras = "lazyvim.plugins.extras"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -58,7 +61,7 @@ require("lazy").setup({
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
 	dev = {
-		path = "~/.ghq/github.com",
+		path = "~/ghq/github.com",
 	},
 	checker = { enabled = true }, -- automatically check for plugin updates
 	performance = {
