fb8ad9fe4feb01b80ff3e61193fa94527cb063ef
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-27 21:35:51 -0400

    feat(utils): add helper_functions modul

---
+++ lua/util/helper_functions.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+-- Helper function to set keymaps with default options and description
+M.keymap = function(mode, lhs, rhs, opts, description)
+	opts = opts or {}
+	opts.noremap = true
+	opts.silent = true
+	opts.desc = description or "No description provided"
+	vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+return M
