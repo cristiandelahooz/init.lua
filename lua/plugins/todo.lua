a04b4c518810483873ec503fd3b700b0f81818b8
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-04-30 07:23:12 -0400

    refactor(todo.nvim): use default options

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -4,24 +4,7 @@ return {
     -- Load lazily to test performance
     event = "VeryLazy",
     config = function()
-      require("todo").setup({
-        -- Custom state directory for testing
-        state_dir = vim.fn.expand("~/.todo-nvim-test/state"),
-        -- Custom todo file for testing
-        todo_file = vim.fn.expand("~/.todo-nvim-test/todo/todos.json"),
-        -- Short auto-deletion time for testing (10 seconds)
-        auto_delete_ms = 10 * 1000,
-        -- Optional: Override colors for testing
-        colors = {
-          orange = "#f97316", -- TailwindCSS orange-500
-          yellow = "#b58900",
-          green = "#859900",
-          base0 = "#839496",
-          base03 = "#002b36",
-        },
-        -- Enable debug logging for development
-        debug = false,
-      })
+      require("todo").setup({})
     end,
     keys = {
       { "<leader>t", "<cmd>TodoOpen<cr>", desc = "Open Todo List" },
