6a06d676f3d5cd96372ef663728950ebc5153c51
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-05-05 02:50:48 -0400

    refactor(colorscheme): downgrade priority

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,10 @@ return {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("solarized-osaka").setup({
+    opts = function()
+      return {
         transparent = true,
-      })
-      vim.cmd.colorscheme("solarized-osaka")
+      }
     end,
   },
 }
