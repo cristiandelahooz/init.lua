844b32a8c3c660f4cd30d088ab225a8cfbf047a0
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-05-10 21:24:25 -0400

    feat(LazyVim): add `editor.mini-file`

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -48,6 +48,7 @@ M.spec = {
   { import = extras .. ".ai.copilot-chat" },
   { import = extras .. ".editor.harpoon2" },
   { import = extras .. ".editor.inc-rename" },
+  { import = extras .. ".editor.mini-files" },
   { import = extras .. ".formatting.biome" },
   { import = extras .. ".util.dot" },
   { import = extras .. ".util.mini-hipatterns" },
